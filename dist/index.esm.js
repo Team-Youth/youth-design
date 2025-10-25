eb99ae18bbf8fa2881cc9d56b95cc86654c5c1f7
Haru-Im <chalk323@naver.com>
2025-10-25 16:03:50 +0900

    svg icon size 24로 강제

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -22187,58 +22187,62 @@ const Toaster = /*#__PURE__*/ React.forwardRef(function Toaster(props, ref) {
 
 // 커스텀 아이콘 컴포넌트들 (Figma 사양: 24x24px)
 var CheckCircleIcon = function (_a) {
-  _a.color;
+  var color = _a.color;
   return jsx("svg", {
     width: "24",
     height: "24",
     viewBox: "0 0 24 24",
     fill: "none",
     xmlns: "http://www.w3.org/2000/svg",
+    className: "youth-toast-icon",
     children: jsx("path", {
       d: "M12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1ZM17.1582 7.99707C16.7426 7.63373 16.1106 7.67628 15.7471 8.0918L10.5859 13.9902L8.24609 11.3682C7.87831 10.9562 7.24597 10.9204 6.83398 11.2881C6.422 11.6559 6.38619 12.2882 6.75391 12.7002L9.84766 16.166C10.0385 16.3798 10.312 16.5014 10.5986 16.5C10.8853 16.4986 11.1579 16.3739 11.3467 16.1582L17.2529 9.4082C17.6163 8.99256 17.5737 8.36065 17.1582 7.99707Z",
-      fill: "#00C785"
+      fill: color
     })
   });
 };
 var CloseCircleIcon = function (_a) {
-  _a.color;
+  var color = _a.color;
   return jsx("svg", {
     width: "24",
     height: "24",
     viewBox: "0 0 24 24",
     fill: "none",
     xmlns: "http://www.w3.org/2000/svg",
+    className: "youth-toast-icon",
     children: jsx("path", {
       d: "M12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1ZM16.0664 7.93359C15.6759 7.54317 15.0428 7.54318 14.6523 7.93359L12 10.585L9.34863 7.93359C8.95812 7.54328 8.32504 7.54321 7.93457 7.93359C7.54411 8.32406 7.54423 8.95712 7.93457 9.34766L10.5859 11.999L7.93457 14.6514C7.54425 15.0419 7.54418 15.675 7.93457 16.0654C8.32505 16.4557 8.95815 16.4557 9.34863 16.0654L12 13.4131L14.6523 16.0654C15.0429 16.4557 15.676 16.4559 16.0664 16.0654C16.4568 15.675 16.4567 15.0419 16.0664 14.6514L13.4141 11.999L16.0664 9.34766C16.4568 8.95718 16.4567 8.3241 16.0664 7.93359Z",
-      fill: "#FF2E2E"
+      fill: color
     })
   });
 };
 var CautionIcon = function (_a) {
-  _a.color;
+  var color = _a.color;
   return jsx("svg", {
     width: "24",
     height: "24",
     viewBox: "0 0 24 24",
     fill: "none",
     xmlns: "http://www.w3.org/2000/svg",
+    className: "youth-toast-icon",
     children: jsx("path", {
       d: "M12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1ZM12 15.25C11.3096 15.25 10.75 15.8096 10.75 16.5C10.75 17.1904 11.3096 17.75 12 17.75C12.6904 17.75 13.25 17.1904 13.25 16.5C13.25 15.8096 12.6904 15.25 12 15.25ZM12 6.25C11.4477 6.25 11 6.69772 11 7.25V13.25C11 13.8023 11.4477 14.25 12 14.25C12.5523 14.25 13 13.8023 13 13.25V7.25C13 6.69772 12.5523 6.25 12 6.25Z",
-      fill: "#FFCC00"
+      fill: color
     })
   });
 };
 var InfoIcon = function (_a) {
-  _a.color;
+  var color = _a.color;
   return jsx("svg", {
     width: "24",
     height: "24",
     viewBox: "0 0 24 24",
     fill: "none",
     xmlns: "http://www.w3.org/2000/svg",
+    className: "youth-toast-icon",
     children: jsx("path", {
       d: "M12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 0.999999 18.0751 1 12C1 5.92487 5.92487 1 12 1ZM12 9.75C11.4477 9.75 11 10.1977 11 10.75V16.75C11 17.3023 11.4477 17.75 12 17.75C12.5523 17.75 13 17.3023 13 16.75V10.75C13 10.1977 12.5523 9.75 12 9.75ZM12 6.25C11.3096 6.25 10.75 6.80964 10.75 7.5C10.75 8.19036 11.3096 8.75 12 8.75C12.6904 8.75 13.25 8.19035 13.25 7.5C13.25 6.80965 12.6904 6.25 12 6.25Z",
-      fill: "#2F6EFF"
+      fill: color
     })
   });
 };
@@ -22270,7 +22274,7 @@ var ToastProvider = function (_a) {
     if (!document.getElementById(styleId)) {
       var style = document.createElement('style');
       style.id = styleId;
-      style.innerHTML = "\n        .youth-toast-title {\n          /* Figma Title/H3 \uC2A4\uD0C0\uC77C */\n          font-family: ".concat(textStyles.heading3.fontFamily, ";\n          font-size: 18px !important;\n          font-weight: ").concat(textStyles.heading3.fontWeight, " !important;\n          line-height: 1.33 !important;\n          color: ").concat(colors.primary.coolGray[800], " !important;\n          letter-spacing: 0 !important;\n          margin: 0 !important;\n          white-space: pre-line !important;\n        }\n        \n        .youth-toast-description {\n          /* Figma body/b2_regular \uC2A4\uD0C0\uC77C */\n          font-family: ").concat(textStyles.body2.fontFamily, ";\n          font-size: 14px !important;\n          font-weight: ").concat(textStyles.body2.fontWeight, " !important;\n          line-height: 1.57 !important;\n          letter-spacing: -0.02em !important;\n          color: ").concat(colors.primary.coolGray[500], " !important;\n          margin: 0 !important;\n          margin-top: 4px !important;\n          white-space: pre-line !important;\n        }\n\n        /* \uD14D\uC2A4\uD2B8 \uCEE8\uD14C\uC774\uB108 \uAC04\uACA9 \uC870\uC815 */\n        [data-sonner-toast] [data-content] {\n          display: flex;\n          flex-direction: column;\n          gap: 4px;\n        }\n      ");
+      style.innerHTML = "\n        .youth-toast-title {\n          /* Figma Title/H3 \uC2A4\uD0C0\uC77C */\n          font-family: ".concat(textStyles.heading3.fontFamily, ";\n          font-size: 18px !important;\n          font-weight: ").concat(textStyles.heading3.fontWeight, " !important;\n          line-height: 1.33 !important;\n          color: ").concat(colors.primary.coolGray[800], " !important;\n          letter-spacing: 0 !important;\n          margin: 0 !important;\n          white-space: pre-line !important;\n        }\n        \n        .youth-toast-description {\n          /* Figma body/b2_regular \uC2A4\uD0C0\uC77C */\n          font-family: ").concat(textStyles.body2.fontFamily, ";\n          font-size: 14px !important;\n          font-weight: ").concat(textStyles.body2.fontWeight, " !important;\n          line-height: 1.57 !important;\n          letter-spacing: -0.02em !important;\n          color: ").concat(colors.primary.coolGray[500], " !important;\n          margin: 0 !important;\n          margin-top: 4px !important;\n          white-space: pre-line !important;\n        }\n\n        /* \uD14D\uC2A4\uD2B8 \uCEE8\uD14C\uC774\uB108 \uAC04\uACA9 \uC870\uC815 */\n        [data-sonner-toast] [data-content] {\n          display: flex;\n          flex-direction: column;\n          gap: 4px;\n        }\n\n        /* \uC544\uC774\uCF58 \uD06C\uAE30 \uAC15\uC81C \uC801\uC6A9 - sonner \uAE30\uBCF8 \uC2A4\uD0C0\uC77C \uC624\uBC84\uB77C\uC774\uB4DC */\n        [data-sonner-toast] [data-icon] svg,\n        [data-sonner-toast] [data-icon] .youth-toast-icon,\n        .youth-toast-icon {\n          width: 24px !important;\n          height: 24px !important;\n          min-width: 24px !important;\n          min-height: 24px !important;\n          max-width: 24px !important;\n          max-height: 24px !important;\n          flex-shrink: 0 !important;\n        }\n\n        /* sonner \uAE30\uBCF8 \uC544\uC774\uCF58 \uCEE8\uD14C\uC774\uB108 \uC2A4\uD0C0\uC77C \uC624\uBC84\uB77C\uC774\uB4DC */\n        [data-sonner-toast] [data-icon] {\n          width: 24px !important;\n          height: 24px !important;\n          min-width: 24px !important;\n          min-height: 24px !important;\n        }\n      ");
       document.head.appendChild(style);
     }
   }, []);
--- src/components/toast/ToastProvider.tsx
+++ src/components/toast/ToastProvider.tsx
@@ -8,37 +8,65 @@ export type ToastStatus = 'success' | 'error' | 'warning' | 'info';
 
 // 커스텀 아이콘 컴포넌트들 (Figma 사양: 24x24px)
 const CheckCircleIcon: React.FC<{ color: string }> = ({ color }) => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="youth-toast-icon"
+  >
     <path
       d="M12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1ZM17.1582 7.99707C16.7426 7.63373 16.1106 7.67628 15.7471 8.0918L10.5859 13.9902L8.24609 11.3682C7.87831 10.9562 7.24597 10.9204 6.83398 11.2881C6.422 11.6559 6.38619 12.2882 6.75391 12.7002L9.84766 16.166C10.0385 16.3798 10.312 16.5014 10.5986 16.5C10.8853 16.4986 11.1579 16.3739 11.3467 16.1582L17.2529 9.4082C17.6163 8.99256 17.5737 8.36065 17.1582 7.99707Z"
-      fill="#00C785"
+      fill={color}
     />
   </svg>
 );
 
 const CloseCircleIcon: React.FC<{ color: string }> = ({ color }) => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="youth-toast-icon"
+  >
     <path
       d="M12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1ZM16.0664 7.93359C15.6759 7.54317 15.0428 7.54318 14.6523 7.93359L12 10.585L9.34863 7.93359C8.95812 7.54328 8.32504 7.54321 7.93457 7.93359C7.54411 8.32406 7.54423 8.95712 7.93457 9.34766L10.5859 11.999L7.93457 14.6514C7.54425 15.0419 7.54418 15.675 7.93457 16.0654C8.32505 16.4557 8.95815 16.4557 9.34863 16.0654L12 13.4131L14.6523 16.0654C15.0429 16.4557 15.676 16.4559 16.0664 16.0654C16.4568 15.675 16.4567 15.0419 16.0664 14.6514L13.4141 11.999L16.0664 9.34766C16.4568 8.95718 16.4567 8.3241 16.0664 7.93359Z"
-      fill="#FF2E2E"
+      fill={color}
     />
   </svg>
 );
 
 const CautionIcon: React.FC<{ color: string }> = ({ color }) => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="youth-toast-icon"
+  >
     <path
       d="M12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1ZM12 15.25C11.3096 15.25 10.75 15.8096 10.75 16.5C10.75 17.1904 11.3096 17.75 12 17.75C12.6904 17.75 13.25 17.1904 13.25 16.5C13.25 15.8096 12.6904 15.25 12 15.25ZM12 6.25C11.4477 6.25 11 6.69772 11 7.25V13.25C11 13.8023 11.4477 14.25 12 14.25C12.5523 14.25 13 13.8023 13 13.25V7.25C13 6.69772 12.5523 6.25 12 6.25Z"
-      fill="#FFCC00"
+      fill={color}
     />
   </svg>
 );
 
 const InfoIcon: React.FC<{ color: string }> = ({ color }) => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="youth-toast-icon"
+  >
     <path
       d="M12 1C18.0751 1 23 5.92487 23 12C23 18.0751 18.0751 23 12 23C5.92487 23 0.999999 18.0751 1 12C1 5.92487 5.92487 1 12 1ZM12 9.75C11.4477 9.75 11 10.1977 11 10.75V16.75C11 17.3023 11.4477 17.75 12 17.75C12.5523 17.75 13 17.3023 13 16.75V10.75C13 10.1977 12.5523 9.75 12 9.75ZM12 6.25C11.3096 6.25 10.75 6.80964 10.75 7.5C10.75 8.19036 11.3096 8.75 12 8.75C12.6904 8.75 13.25 8.19035 13.25 7.5C13.25 6.80965 12.6904 6.25 12 6.25Z"
-      fill="#2F6EFF"
+      fill={color}
     />
   </svg>
 );
@@ -129,6 +157,27 @@ export const ToastProvider: React.FC<ToastProviderProps> = ({
           display: flex;
           flex-direction: column;
           gap: 4px;
+        }
+
+        /* 아이콘 크기 강제 적용 - sonner 기본 스타일 오버라이드 */
+        [data-sonner-toast] [data-icon] svg,
+        [data-sonner-toast] [data-icon] .youth-toast-icon,
+        .youth-toast-icon {
+          width: 24px !important;
+          height: 24px !important;
+          min-width: 24px !important;
+          min-height: 24px !important;
+          max-width: 24px !important;
+          max-height: 24px !important;
+          flex-shrink: 0 !important;
+        }
+
+        /* sonner 기본 아이콘 컨테이너 스타일 오버라이드 */
+        [data-sonner-toast] [data-icon] {
+          width: 24px !important;
+          height: 24px !important;
+          min-width: 24px !important;
+          min-height: 24px !important;
         }
       `;
       document.head.appendChild(style);
